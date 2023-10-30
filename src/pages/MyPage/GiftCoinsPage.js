da44450d84410cf00d4a12554b2cf4db104428aa
Billy Lights <alpha5611331@gmail.com>
2023-10-30 19:12:00 +0900

    fixing memory bugs ...

--- src/pages/MyPage/GiftCoinsPage.js
+++ src/pages/MyPage/GiftCoinsPage.js
@@ -64,4 +64,4 @@ export const ProfileGiftCoinsPage = () => {
     </LbPageTransition>
   )
 }
-            
+             
--- src/pages/layout/Navbar.js
+++ src/pages/layout/Navbar.js
@@ -168,4 +168,4 @@ export const LbNavbar = () => {
     </header >
   )
 }
-                   
+                    
